69a22626a569a61e512351e3abf078bbe3f1fbe3
Rakesh kumar Elumalai <rkshjab@gmail.com>
2024-04-02 23:31:47 +0530

    Arraymethods.task

--- Task7.ArrayMethods/script.js
+++ Task7.ArrayMethods/script.js
@@ -2,24 +2,20 @@
 fetch("https://restcountries.com/v3.1/all")
   .then(response => response.json())
   .then(data => {
-    // Get all countries from Asia continent using Filter function
+    // a. Get all countries from Asia continent using Filter function
     const asiaCountries = data.filter(country => country.region === "Asia");
     console.log("Countries from Asia continent:", asiaCountries);
 
-    // Get all countries with a population of less than 2 lakhs using Filter function
+    // b. Get all countries with a population of less than 2 lakhs using Filter function
     const lessThan2LakhPopulation = data.filter(country => country.population < 200000);
     console.log("Countries with population less than 2 lakhs:", lessThan2LakhPopulation);
 
-    // Print name, capital, and flag of each country using forEach function
+    // c. Print name, capital, and flag of each country using forEach function
     data.forEach(country => {
       console.log("Name:", country.name.common, ", Capital:", country.capital[0], ", Flag:", country.flags.svg);
     });
 
-    // Print total population of countries using reduce function
-    const totalPopulation = data.reduce((acc, country) => acc + country.population, 0);
-    console.log("Total population of countries:", totalPopulation);
-
-    // Print the country that uses US dollars as currency
+    // e. Print the country that uses US dollars as currency
     const countryWithUSD = data.find(country => {
       const currencies = Object.values(country.currencies);
       return currencies.includes("USD");
@@ -28,4 +24,14 @@ fetch("https://restcountries.com/v3.1/all")
   })
   .catch(error => console.error("Error fetching data:", error));
 
+  // d. Print total population of countries using reduce function
+
+fetch("https://restcountries.com/v3.1/all")
+.then(response => response.json())
+.then(data => {
+  // Calculate the total population using reduce function
+  const totalPopulation = data.reduce((acc, country) => acc + country.population, 0);
+  console.log("Total population of countries:", totalPopulation);
+})
+.catch(error => console.error("Error fetching data:", error));
   
